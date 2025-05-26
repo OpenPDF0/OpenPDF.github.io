14308fffdce191bd8d20541b6c6e3747dce36b3e
OpenPDF0 <Openpdf0@gmail.com>
2025-05-26 17:16:15 -0300

    Update script.py

--- script.py
+++ script.py
@@ -556,4 +556,4 @@ if __name__ == '__main__':
         lixeira_cleaner_thread.start()
 
     # debug=True ativa o modo de desenvolvimento e o auto-reload
-    app.run(debug=True)
+    app.run(debug=False)
